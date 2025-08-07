a096b3216f3ad781a7ac5e7e50547fdc09a04f21
Kosaka Bun <kosaka-bun@qq.com>
2025-08-07 16:54:41 +0800

    Update gradle/library/publish.sh

--- scripts/gradle/library/publish.sh
+++ scripts/gradle/library/publish.sh
@@ -38,10 +38,11 @@ cd ..
 # 将[项目根目录]/maven-repo-changes/maven-repo/repository下所有内容，复制到[项目根目录]/maven-repo
 # /repository下，并替换已存在的内容。
 #
-cp -rf maven-repo-changes/maven-repo/repository/* maven-repo/repository/
+cp -rf maven-repo-changes/maven-repo/repository/. maven-repo/repository/
 
 # 进入存储Maven仓库文件的Git仓库，设置提交者信息，然后提交并推送
 cd maven-repo/repository
+date > update_time.txt
 
 commit_message="Update $PROJECT_NAME"
 if [ "$IS_DEVELOPMENT_VERSION" != 'false' ]; then
